8c6aa37a384859a9215c1d924f5b6a488297baed
Francisco Batalha <you@example.com>
2025-01-14 21:14:51 -0300

    up

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Embalagem(models.Model):
     tipo = models.CharField(max_length=50)
@@ -31,4 +32,84 @@ class TipoSabor(models.Model):
         verbose_name = '2 - TipoSabor'
         verbose_name_plural = '2 - TipoSabor'
 
-        #a
+class Sabor(models.Model):
+    nome = models.CharField(max_length=50)
+    tipo = models.ForeignKey(TipoSabor, related_name='tipo_sabor', on_delete=models.CASCADE) # Tradicional
+    ativo = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.nome} | PREÇO: R$ {self.tipo.preco:.2f}'
+
+    class Meta:
+        verbose_name = '3 - Sabor'
+        verbose_name_plural = '3 - Sabor'
+
+class Cobertura(models.Model):
+    nome = models.CharField(max_length=50)
+    ativo = models.BooleanField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def preco_formatado(self):
+        return f'R$ {self.preco:.2f}'
+
+    def __str__(self):
+        return f'{self.nome} | PREÇO: R$ {self.preco:.2f}'
+
+    class Meta:
+        verbose_name = '4 - Cobertura'
+        verbose_name_plural = '4 - Cobertura'
+
+
+# POTE 1L 6 SABORES DE CHOCOLATE. X3
+
+class MontaPote(models.Model):
+    embalagem = models.ForeignKey(Embalagem, related_name='embalagem', on_delete=models.CASCADE, null=True)
+    coberturas = models.ManyToManyField(Cobertura)
+    quantidade = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f"ID: {self.id} / POTE: {self.embalagem.tipo} / Qtd: {self.quantidade}"
+
+    class Meta:
+        verbose_name = 'B - MontaPote'
+        verbose_name_plural = 'B - MontaPote'
+
+class SelSabor(models.Model):
+    pote = models.ForeignKey(MontaPote, related_name='pote', on_delete=models.CASCADE, null=True)
+    sabor = models.ForeignKey(Sabor, related_name='sabor', on_delete=models.CASCADE, null=True)
+    quantidade_bolas = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Sabor: {self.sabor.nome}, Quantidade de Bolas: {self.quantidade_bolas}"
+
+    class Meta:
+        verbose_name = 'A - SelSabor'
+        verbose_name_plural = 'A - SelSabor'
+
+class SacolaItens(models.Model):
+    potes = models.ManyToManyField(MontaPote)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+
+    def preco_formatado(self):
+        return f'R$ {self.preco:.2f}'
+
+    def __str__(self):
+        return f"CARINHO: {self.id}"
+
+    class Meta:
+        verbose_name = 'C - SacolaItens'
+        verbose_name_plural = 'C - SacolaItens'
+
+class Pedido(models.Model):
+    data_pedido = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(User, related_name='pedido_user', on_delete=models.PROTECT)
+    itens_da_sacola = models.OneToOneField(SacolaItens, on_delete=models.CASCADE, null=True)
+    status = models.BooleanField()
+    pago = models.BooleanField()
+
+    def __str__(self):
+        return f"Pedido: {self.id} / {self.user} / (PAGO: {self.pago})"
+
+    class Meta:
+        verbose_name = 'D - Pedido'
+        verbose_name_plural = 'D - Pedido'
